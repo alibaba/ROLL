341203ae115a9d067c118e8fa5e8c8130e75b3da
tianhe.lzd <tianhe.lzd@alibaba-inc.com>
2025-09-05 15:50:08 +0800

    (feat): support sglang 0.4.10.post2.

--- roll/third_party/sglang/__init__.py
+++ roll/third_party/sglang/__init__.py
@@ -7,5 +7,8 @@ if sgl.__version__ == '0.4.6.post4':
 elif sgl.__version__ == '0.4.3.post4':
     from roll.third_party.sglang import v043post4_patch
     patch = v043post4_patch
+elif sgl.__version__ == '0.4.10.post2':
+    from roll.third_party.sglang import v0410post2_patch
+    patch = v0410post2_patch
 else:
      raise NotImplementedError(f"Scale aligner version sglang:{sgl.__version__} is not supported.")
