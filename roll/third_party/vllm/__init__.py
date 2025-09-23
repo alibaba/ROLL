c4ac728919a7a802e0340e7107ee57c4cca58659
huangju.hj <huangju.hj@alibaba-inc.com>
2025-09-23 18:13:47 +0800

    fix: fix vllm version compare

--- roll/third_party/vllm/__init__.py
+++ roll/third_party/vllm/__init__.py
@@ -13,7 +13,7 @@ if Version("0.8.4") == Version(vllm.__version__):
     from roll.third_party.vllm.vllm_0_8_4.v1.async_llm import AsyncLLM084
     LLM = Llm084
     AsyncLLM = AsyncLLM084
-elif Version("0.10.0") == Version(vllm.__version__):
+elif Version("0.10.0") <= Version(vllm.__version__) < Version("0.10.2"):
     from roll.third_party.vllm.vllm_0_10_0.llm import Llm0100
     from roll.third_party.vllm.vllm_0_10_0.v1.async_llm import AsyncLLM0100
     LLM = Llm0100
